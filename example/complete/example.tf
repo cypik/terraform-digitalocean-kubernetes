26a6b053d30dada22fd76ce6b70b67d0b9e3df66
chandrahaskoneru <123540688+chandrahaskoneru@users.noreply.github.com>
2023-11-25 09:59:34 +0530

    Update example.tf

--- example/complete/example.tf
+++ example/complete/example.tf
@@ -12,7 +12,7 @@ locals {
 ## VPC module call
 ##------------------------------------------------
 module "vpc" {
-  source      = "git::https://github.com/opz0/terraform-digitalocean-vpc.git?ref=v1.0.0"
+  source      = "git::https://github.com/cypik/terraform-digitalocean-vpc.git?ref=v1.0.0"
   name        = "digitalocen-vpc-2"
   environment = local.environment
   region      = local.region
